1e84631bb0fd49916d729337751c4e84bb54545d
Schudel, MJ (Michel) <m.j.schudel@rn.rabobank.nl>
2017-09-23 16:13:42 +0200

    Rename variables

--- src/main/java/nl/craftsmen/moviestore/Customer.java
+++ src/main/java/nl/craftsmen/moviestore/Customer.java
@@ -16,52 +16,52 @@ public class Customer {
     }
 
     public String getRentalOverview() {
-        double t = 0;
-        int p = 0;
-        StringBuilder b = new StringBuilder();
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        StringBuilder overviewBuilder = new StringBuilder();
 
         // Add header to overview
-        b.append("Rental overview for " + name + "\n\n");
-        for (Rental r : rentals) {
-            double a = 0;
+        overviewBuilder.append("Rental overview for " + name + "\n\n");
+        for (Rental rental : rentals) {
+            double amount = 0;
 
             // Determine amounts for each line
-            switch (r.getMovie().getPriceCode()) {
+            switch (rental.getMovie().getPriceCode()) {
                 case CHILDRENS:
-                    a += 1.5;
-                    if (r.getDaysRented() > 3) {
-                        a += (r.getDaysRented() - 3) * 1.5;
+                    amount += 1.5;
+                    if (rental.getDaysRented() > 3) {
+                        amount += (rental.getDaysRented() - 3) * 1.5;
                     }
                     break;
                 case NEW_RELEASE:
-                    a += r.getDaysRented() * 3;
+                    amount += rental.getDaysRented() * 3;
                     break;
                 case REGULAR:
-                    a += 2;
-                    if (r.getDaysRented() > 2) {
-                        a += (r.getDaysRented() - 2) * 1.5;
+                    amount += 2;
+                    if (rental.getDaysRented() > 2) {
+                        amount += (rental.getDaysRented() - 2) * 1.5;
                     }
                     break;
             }
 
             // Add frequent renter points
-            p += 1;
+            frequentRenterPoints += 1;
 
             // Add bonus for a two day new release rental
-            if (r.getMovie().getPriceCode() == PriceCode.NEW_RELEASE && r.getDaysRented() > 1) {
-                p++;
+            if (rental.getMovie().getPriceCode() == PriceCode.NEW_RELEASE && rental.getDaysRented() > 1) {
+                frequentRenterPoints++;
             }
 
             // Add rental to overview
-            b.append(String.format("%-20s %5.2f\n", r.getMovie().getTitle(), a));
-            t += a;
+            overviewBuilder.append(String.format("%-20s %5.2f\n", rental.getMovie().getTitle(), amount));
+            totalAmount += amount;
         }
 
         // Add footer to overview
-        b.append("==========================\n");
-        b.append(String.format("%-20s %5.2f\n", "Total", t));
-        b.append("\n");
-        b.append("You earned " + p + " frequent renter points\n");
-        return b.toString();
+        overviewBuilder.append("==========================\n");
+        overviewBuilder.append(String.format("%-20s %5.2f\n", "Total", totalAmount));
+        overviewBuilder.append("\n");
+        overviewBuilder.append("You earned " + frequentRenterPoints + " frequent renter points\n");
+        return overviewBuilder.toString();
     }
 }
